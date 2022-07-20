af21c50fe4a3462dbe3fc6e997fbdad989516fa8
lompdoctor <pleclair@protonmail.com>
2022-07-20 16:47:23 -0500

    Add state to top/bottomText, style text

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -3,7 +3,19 @@ import "./Meme.css"
 import memesData from "../memesData"
 
 export default function Meme() {
+    const [textData, setTextData] = React.useState({
+        topText: "",
+        bottomText: ""
+    })
     const [memeUrl, setMemeUrl] = React.useState("");
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setTextData(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
 
     function getMeme() {
         const index = Math.floor(Math.random()*memesData.data.memes.length-1)
@@ -14,11 +26,25 @@ export default function Meme() {
         <div className="meme--container">
             <div className="form">
                 <div>
-                    <input placeholder="Top text"></input>
-                    <input placeholder="Bottom text"></input>
+                    <input 
+                        placeholder="Top text"
+                        name="topText"
+                        value={textData.topText}
+                        onChange={handleChange}
+                    />
+                    <input 
+                        placeholder="Bottom text" 
+                        name="bottomText"
+                        value={textData.bottomText}
+                        onChange={handleChange}
+                    />
                 </div>
                 <button onClick={getMeme}>Get a new meme image 🖼</button>
-                <img className="meme--image" src={memeUrl} alt="Meme"/>
+                <div className="meme--image" style={{backgroundImage: `url(${memeUrl})`}}>
+                    <div className="meme--topText">{textData.topText}</div>
+                    {/* <img className="meme--image" src={memeUrl} alt="Meme"/> */}
+                    <div className="meme--bottomText">{textData.bottomText}</div>
+                </div>
             </div>
         </div>
     )
